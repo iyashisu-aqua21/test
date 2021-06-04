31ad9966468a165e26214d7dfcf47c0ba7bc38f6
iyashisu-aqua21 <85287393+iyashisu-aqua21@users.noreply.github.com>
2021-06-04 14:14:48 +0900

    Add files via upload

---
+++ js/news.js
@@ -0,0 +1,8 @@
+$(function(){
+		
+	var url = location.href;
+	var params = $('#ifr').attr('name');
+	url.match('iyashisu.com');{
+		$('#ifr').attr("src", "http://www17082ui.sakura.ne.jp/?shop_id="+params);
+	}
+});
